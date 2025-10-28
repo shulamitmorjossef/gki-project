be8b449f9153f5cc5d53b87d0a8c52b58b9e89dd
shulamitmorjossef <shulamitmorjossef@gmail.com>
2025-10-28 14:18:16 +0200

    added Header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,63 @@
+// "use client";
+
+// import React from "react";
+// import styles from "../styles/Header.module.css";
+
+// export default function Header() {
+//   return (
+//     <header className={styles.header}>
+//       <div className={styles.left}>
+//         <img
+//           src="/bee-logo.png" 
+//           alt="Bee Logo"
+//           className={styles.logo}
+//         />
+//         <span className={styles.shopText}>Shop</span>
+//       </div>
+
+//       <nav className={styles.center}>
+//        <a href="/category/men">Men</a>
+//        <a href="/category/women">Women</a>
+//        <a href="/category/jewelry">Jewelry</a>
+//        <a href="/category/electronics">Electronics</a>
+//       </nav>
+
+//       <div className={styles.right}>
+//         <a href="/">Cart</a>
+//       </div>
+//     </header>
+//   );
+// }
+
 "use client";
 
-import React from "react";
+import React, { JSX } from "react";
 import styles from "../styles/Header.module.css";
 
-export default function Header() {
+export default function Header(): JSX.Element {
   return (
     <header className={styles.header}>
-      <div className={styles.left}>
-        <img
-          src="/bee-logo.png" 
-          alt="Bee Logo"
-          className={styles.logo}
-        />
-        <span className={styles.shopText}>Shop</span>
-      </div>
+     <div className={styles.left}>
+         <img
+         src="/bee-logo.png" 
+         alt="Bee Logo"
+         className={styles.logo}
+         />
+         <span className={styles.shopText}>Shop</span>
+     </div>
 
-      <nav className={styles.center}>
-       <a href="/category/men">Men</a>
-       <a href="/category/women">Women</a>
+       <nav className={styles.center}>
+       {/* <a href="/" className={styles.activeLink}>Home</a> */}
+       <a href="/">Home</a>
+       <a href="/category/men">Mens</a>
+       <a href="/category/women">Womens</a>
        <a href="/category/jewelry">Jewelry</a>
        <a href="/category/electronics">Electronics</a>
-      </nav>
+       {/* <a href="/contact">Contact Us</a> */}
+       </nav>
 
       <div className={styles.right}>
-        <a href="/">Cart</a>
+       <a href="/cart">Cart</a>
       </div>
     </header>
   );
